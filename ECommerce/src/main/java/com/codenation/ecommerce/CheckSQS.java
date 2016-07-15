1c3abcd013a3174f3de81ec5578b801aa332291d
Ferooz Khan <ferooz@codenation.co.in>
2016-07-15 10:25:21 +0400

    Assignment04c

---
+++ ECommerce/src/main/java/com/codenation/ecommerce/CheckSQS.java
@@ -0,0 +1,18 @@
+package com.codenation.ecommerce;
+
+import com.codenation.ecommerce.Helpers.AWSMessagingService;
+import com.codenation.ecommerce.Pojos.LogInformation;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Ferooz on 11/07/16.
+ */
+public class CheckSQS {
+    public static void main(String args[])
+    {
+        AWSMessagingService awsMessagingService = new AWSMessagingService();
+
+
+    }
+}
